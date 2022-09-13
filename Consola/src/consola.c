3ad6120a277ee4f1e88fff3ccbe2fd54f273286d
MatiasRosbaco <mati.rosbaco@gmail.com>
2022-09-13 00:10:54 +0000

    conexion consola a kernel y agregando librerias

--- Consola/src/consola.c
+++ Consola/src/consola.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
+#include "../../Libreria/tcp_client.c"
+#include "commons/log.h"
+#include "../../Libreria/tcp_serializacion.c"
+#include "commons/config.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
 
-int main(){
-    printf("hola mundo\n");
+int main(int argc, char** argv[]){
+    //archivo config
+    
+    t_config* config = config_create(argv[0]);
+    //file* archivo= fopen(argv[1],) abrir archivo en argv 1 o 2
+    //parseo de lineas de comando, 
+    
+    int socket = crear_conexion("127.0.0.1","8000");
+    //enviar lineas parseadas y serializadas
+    
+    recibir_operacion(socket);//se queda esperando mensajes
+
     return 0;
 }
+
+
+
+
--- Kernel/src/kernel.c
+++ Kernel/src/kernel.c
@@ -1,3 +1,42 @@
+#include "../../Libreria/tcp_server.c"
+#include<pthread.h> 
+#include "commons/log.h"
+#include "commons/config.h"
+
 int main(){
+
+    pthread_t memoria,cpu_dispatch,cpu_interrupt;
+    t_config* config;
+    config = config_create("kernel.config");
+    if(pthread_create( &memoria, NULL,  memoria_routine, (void*) &config) < 0 ){
+        perror("Error: Memoria thread failed.");//cambiar a los logs
+    }
+    if(pthread_create( &memoria, NULL,  cpu_dispatch_routine, (void*) &config) < 0){
+        perror("Error: Cpu_dispatch thread failed.");//cambiar a los logs
+    }
+    if(pthread_create( &memoria, NULL,  cpu_interrupt_routine, (void*) &config) < 0){
+        perror("Error: Cpu_interrupt thread failed.");
+    }
+
+    iniciar_servidor("127.0.0.1","8000",consola_routine);
+
     return 0;
+}
+void* consola_routine(void* socket){
+    int socket_cliente = *(int*) socket;
+    
+    //to do escuchar--desearlizacion
+    close(socket_cliente);
+    return 0;
+}
+
+void* memoria_routine(void* config){
+
+}
+
+void* cpu_dispatch_routine(void* config){
+
+}
+void* cpu_interrupt_routine(void* config){
+
 }
