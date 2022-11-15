c7b02a99e4d4ead3db4218ca637c7272279e80ce
CFedericoDavid <caputo.d.federico@gmail.com>
2022-11-15 20:04:06 +0000

    Agrego tamanio de segmento a las funciones de levantar tablas de paginas

--- Memoria/src/paginacion.h
+++ Memoria/src/paginacion.h
@@ -3,16 +3,16 @@
 
 #include <commons/collections/list.h>
 #include <commons/bitarray.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include "memoria_utils.h"
 #include "memoria_globals.h"
 
 //Crea una tabla y devuelve su id
-uint32_t pag_crearTablaPaginas(uint32_t idProceso);
-void pag_liberarTablasPaginas(uint32_t idProceso);
+uint32_t pag_crearTablaPaginas(uint32_t idProceso, uint32_t tamanioSegmento);
+void pag_liberarTablasProceso(uint32_t idProceso);
 int pag_obtenerMarcoPagina(uint32_t idTablaPaginas, uint32_t idPagina, uint32_t *marco);
 void pag_destruirTablaPaginas(void *tabla);
 void pag_destruirPagina(void *pag);
+uint32_t frame_asignar_libre(t_bitarray *mapaFrames);
 
 #endif /* PAGINACION_H_ */
