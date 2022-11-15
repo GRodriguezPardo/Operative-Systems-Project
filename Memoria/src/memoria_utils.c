c48067087b894607d1c92bc087a3ad3fc860e8d1
CFedericoDavid <caputo.d.federico@gmail.com>
2022-11-15 20:12:05 +0000

    refactor de funciones de bitmap

--- Memoria/src/memoria_utils.c
+++ Memoria/src/memoria_utils.c
@@ -1,5 +1,10 @@
 #include "memoria_utils.h"
 
+static uint32_t buscar_posicion_libre(t_bitarray *bitmap, uint32_t longitudMapa);
+static bool posicion_esta_asignada(t_bitarray *bitmap, int frame);
+static void marcar_posicion_asignada(t_bitarray *bitmap, int marco);
+
+
 void esperar_hilos(){
 	pthread_mutex_init(&mx_main, NULL);
 	pthread_mutex_lock(&mx_main);
@@ -49,6 +54,36 @@ t_list *get_tablas_proceso(uint32_t _idProceso){
         t_tablaPaginas *tabla = (t_tablaPaginas *)item;
         return tabla->idProceso == _idProceso;
     }
-    lista = list_filter(espacioTablasPag, _esTablaDelProceso);
+    lista = list_filter(EspacioTablasPag, _esTablaDelProceso);
     return lista;
+}
+
+int sonIguales(const char* str1, const char* str2){
+    return !strcmp(str1, str2);
+}
+
+uint32_t asignar_posicion_libre(t_bitarray *bitmap, uint32_t longitudMapa){
+    uint32_t pos = buscar_posicion_libre(bitmap, longitudMapa);
+    marcar_posicion_asignada(bitmap, pos);
+    return pos;
+}
+
+static uint32_t buscar_posicion_libre(t_bitarray *bitmap, uint32_t longitudMapa){
+    uint32_t posLibre = UINT32_MAX;
+    for (uint32_t i = 0; i < longitudMapa; i++)
+    {
+        if(!posicion_esta_asignada(bitmap, i)){
+            posLibre = i;
+            break;
+        }
+    }
+    return posLibre;
+}
+
+static bool posicion_esta_asignada(t_bitarray *bitmap, int frame){
+    return bitarray_test_bit(bitmap, frame);
+}
+
+static void marcar_posicion_asignada(t_bitarray *bitmap, int frame){
+    bitarray_set_bit(bitmap, frame);
 }
